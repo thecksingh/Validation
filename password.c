565157442568cb240af00c9701310d2c31c822dc
thecksingh <ckindia789@gmail.com>
2017-02-08 07:49:18 +0530

    Create password.c

---
+++ password.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include"https://github.com/thecksingh/Validation/master/header.h"
+void getpassword(char a[]);
+main()
+{
+char a[50];
+printf("enter the password");
+scanf("%s",a);
+getpassword(a);
+}
